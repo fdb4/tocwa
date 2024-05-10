793a7cf2734b558345b710b2eb25568c5cf53cc4
fdb4 <144566244+fdb4@users.noreply.github.com>
2024-05-10 18:33:25 -0400

    Add files via upload

---
+++ databasestuff/categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO tocd.itemlistings_category (categoryID,categoryName)
+VALUES
+(1,'Snacks'),
+(2,'Ground Provision'),
+(3,'Can Foods'),
+(4,'Misc'),
+(5,'Seasoning'),
+(6,'Meat'),
+(7,'Juice/Tea');
